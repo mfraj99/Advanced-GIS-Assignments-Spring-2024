b26e4dea1e2f96dc387aa83db634faac33f522c5
Michael Frajman <michael.frajman@mail.mcgill.ca>
2024-03-27 22:48:43 -0400

    need to fix button click

---
+++ Assignment1/a1.js
@@ -0,0 +1,9 @@
+//https://jsfiddle.net/cwhong/wzgvL35q/?fbclid=IwAR1SHdXfkhSwqa6zunYZ6Cji8IxVWmF5M3qysfJOc0oPW3TIhDksI9pYoLk_aem_Ac4M6SPYl5zfCrGUYu0HPVEJYDv1COf09F0Mjn1PEmx7B7cgNYY8IcrKgExfIYjQ_vv5r9WZNaq_N1c098cf38Cz
+
+$('.Manhattan').on('click', function() {
+    $('body').css('background-color', 'steelblue')
+  })
+
+$('.Staten').click(function(){
+    window.location.href='https://www.nyc.gov/site/statenislandcb1/index.page';
+ })
