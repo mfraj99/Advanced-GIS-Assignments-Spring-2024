40d2a0d26eee5e185a0c9111ffc601e5d9a6e183
Michael Frajman <mhf420@gmail.com>
2024-03-28 19:32:42 -0400

    button click links fixed

--- Assignment1/a1.js
+++ Assignment1/a1.js
@@ -1,9 +1,21 @@
-//https://jsfiddle.net/cwhong/wzgvL35q/?fbclid=IwAR1SHdXfkhSwqa6zunYZ6Cji8IxVWmF5M3qysfJOc0oPW3TIhDksI9pYoLk_aem_Ac4M6SPYl5zfCrGUYu0HPVEJYDv1COf09F0Mjn1PEmx7B7cgNYY8IcrKgExfIYjQ_vv5r9WZNaq_N1c098cf38Cz
+//on clicking a borough button, go to the webpage of that borough
 
-$('.Manhattan').on('click', function() {
-    $('body').css('background-color', 'steelblue')
-  })
+$("#Manhattan").on("click", function () {
+  window.open("https://www.manhattanbp.nyc.gov/", '_blank')
+})
 
-$('.Staten').click(function(){
-    window.location.href='https://www.nyc.gov/site/statenislandcb1/index.page';
- })
+$("#Brooklyn").on("click", function () {
+  window.open("https://www.brooklynbp.nyc.gov/", '_blank')
+})
+
+$("#Bronx").on("click", function () {
+  window.open("https://bronxboropres.nyc.gov/", '_blank')
+})
+
+$("#Queens").on("click", function () {
+  window.open("https://queensbp.org/", '_blank')
+})
+
+$("#Staten").click(function () {
+  window.open("https://www.statenislandusa.com/", '_blank')
+})
